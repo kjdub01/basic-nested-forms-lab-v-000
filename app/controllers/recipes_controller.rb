00ebedfde935aca504e5c7fe327dd10a01e3eedb
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-02-19 03:48:42 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,6 +21,6 @@ class RecipesController < ApplicationController
   private
   
   def recipe_params
-    params.require(:recipe).permit(:title, ingredient_attributes[])
+    params.require(:recipe).permit(:title, ingredient_attributes: [:name, :quantity])
   end
 end
